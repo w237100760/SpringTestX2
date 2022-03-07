4b108d77d7714faf698eb4c72a576dee9043bf47
wck <237100760@qq.com>
2022-03-07 17:16:22 +0800

    Aop dynamic proxy

--- src/main/java/com/springboot/springtestx2/Aop/AopTest.java
+++ src/main/java/com/springboot/springtestx2/Aop/AopTest.java
@@ -10,8 +10,19 @@ public class AopTest {
 
     public static void main(String[] args) {
         HelloService helloService = new HelloServiceImpl();
-        //将服务类和拦截方法织入对应的流程
+        //将服务类和拦截方法织入，生成代理类实例
         HelloService proxy = (HelloService) ProxyBean.getProxyBean(helloService, new myInterceptor());
         proxy.sayHello("kkk");
     }
 }
+
+/*
+* JDK动态代理详解： https://zhuanlan.zhihu.com/p/347141071
+*
+* 1.实现InvocationHandler接口,创建调用请求处理器
+* 2.创建被代理的对象
+* 3.根据2获取其ClassLoader和所有接口的Class(即不同Interface生成的.Class文件)
+* 4.调用newInstance()创建代理实例 (需要1的调用请求处理器,3的类加载器和类文件)
+*
+* proxy.sayHello("kkk");会通过ProxyBean的invoke方法去调用, 最终使用的是method.invoke(target, args);
+* */
--- src/main/java/com/springboot/springtestx2/Aop/Interceptor/ProxyBean.java
+++ src/main/java/com/springboot/springtestx2/Aop/Interceptor/ProxyBean.java
@@ -2,14 +2,13 @@ package com.springboot.springtestx2.Aop.Interceptor;
 
 import com.springboot.springtestx2.Aop.Interceptor.definition.Interceptor;
 import com.springboot.springtestx2.Aop.Interceptor.definition.Invocation;
-
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
 public class ProxyBean implements InvocationHandler {
-    private Object target;
-    private Interceptor interceptor;
+    private Object target;//被代理的对象
+    private Interceptor interceptor;//拦截器
 
     private ProxyBean() {
     }
@@ -36,13 +35,17 @@ public class ProxyBean implements InvocationHandler {
         return instance;
     }*/
 
+    /**
+     * @param target 被代理对象
+     * @param interceptor 拦截代理对象的拦截器
+     * @return 返回代理实例
+     */
     public static Object getProxyBean(Object target, Interceptor interceptor){
-//        ProxyBean proxyBean = ProxyBean.getInstance();
-//        proxyBean.target = target;
-//        proxyBean.interceptor = interceptor;
         ProxyBean proxyBean = new ProxyBean();
+        //将代理类传给调用请求处理器，处理所有的代理对象上的方法调用
         proxyBean.target = target;
         proxyBean.interceptor = interceptor;
+        //由target的ClassLoader和Interfaces确定的代理实例，并交给调用逻辑处理器ProxyBean实现功能增强
         return Proxy.newProxyInstance(target.getClass().getClassLoader(),
                 target.getClass().getInterfaces(), proxyBean);
     }
@@ -50,18 +53,18 @@ public class ProxyBean implements InvocationHandler {
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) {
         boolean exceptionFlag = false;
-        Invocation invocation = new Invocation(target, method, args);
+        Invocation invocation = new Invocation(target, method, args);//保存target 代理对象，method 代理对象的方法，args 代理对象的运行参数
         Object retObj = null;
         try {
             if (this.interceptor.before()){
-                retObj = this.interceptor.around(invocation);
+                retObj = this.interceptor.around(invocation);//method.invoke(target, args);
             } else {
                 retObj = method.invoke(target, args);
             }
         } catch (Exception e){
             exceptionFlag = true;
         }
-        this.interceptor.after();;
+        this.interceptor.after();
         if (exceptionFlag){
             this.interceptor.afterThrowing();
         } else {
