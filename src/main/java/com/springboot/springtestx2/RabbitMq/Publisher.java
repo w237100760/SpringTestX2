7d794fd6a8c92e8d16fcd16c7396304de336dc99
wck <237100760@qq.com>
2022-03-16 20:30:04 +0800

    rabbitmq temp-1

--- src/main/java/com/springboot/springtestx2/RabbitMq/Publisher.java
+++ src/main/java/com/springboot/springtestx2/RabbitMq/Publisher.java
@@ -1,6 +1,7 @@
 package com.springboot.springtestx2.RabbitMq;
 
 import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.ConfirmCallback;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.springboot.springtestx2.RabbitMq.config.RabbitChannel;
@@ -8,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.concurrent.ConcurrentNavigableMap;
+import java.util.concurrent.ConcurrentSkipListMap;
 import java.util.concurrent.TimeoutException;
 
 @Component
@@ -26,11 +29,27 @@ public class Publisher {
             channel.exchangeDeclare(rabbitChannel.EXCHANGE_NAME,RabbitChannel.EXCHANGE_TYPE);
             /*Handling Publisher Confirms Asynchronously*/
             channel.confirmSelect();//This method must be called on every channel that you expect to use publisher confirms
-            channel.addConfirmListener((sequenceNumber, multiple) -> {
-                // code when message is confirmed
-            }, (sequenceNumber, multiple) -> {
-                // code when message is nack-ed
+            ConcurrentNavigableMap<Long, String> outstandingConfirms = new ConcurrentSkipListMap<>();
+            ConfirmCallback cleanOutstandingConfirms = (sequenceNumber, multiple) -> {
+                if (multiple) {
+                    ConcurrentNavigableMap<Long, String> confirmed = outstandingConfirms.headMap(
+                            sequenceNumber, true
+                    );
+                    confirmed.clear();
+                } else {
+                    outstandingConfirms.remove(sequenceNumber);
+                }
+            };
+
+            channel.addConfirmListener(cleanOutstandingConfirms, (sequenceNumber, multiple) -> {
+                String body = outstandingConfirms.get(sequenceNumber);
+                System.err.format(
+                        "Message with body %s has been nack-ed. Sequence number: %d, multiple: %b%n",
+                        body, sequenceNumber, multiple
+                );
+                cleanOutstandingConfirms.handle(sequenceNumber, multiple);
             });
+            outstandingConfirms.put(channel.getNextPublishSeqNo(), msg);
             channel.basicPublish(rabbitChannel.EXCHANGE_NAME, rabbitChannel.ROUTING_KEY, null, msg.getBytes("UTF-8"));
         } catch (IOException | TimeoutException e) {
             e.printStackTrace();
