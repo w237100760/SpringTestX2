cee5547aabec6f146672cc7687108be1ef814e0f
wck <237100760@qq.com>
2022-03-22 16:36:36 +0800

    rabbitmq

--- src/main/java/com/algorithm/LinkedList.java
+++ src/main/java/com/algorithm/LinkedList.java
@@ -61,10 +61,10 @@ public class LinkedList {
     输出的Map有四个kv键值对（->之前为key，之后为value）
     outputString: Map<k1->v1, k2->v2, k3->v3, k4->v41,v42>
     */
-    public static int lastIndex2 = 0;
 
     public static Map<String, String> extract(String inputString) {
         Matcher matcherKey = Pattern.compile("(?<!\")(k\\d=)").matcher(inputString);
+        int lastIndex2 = 0;
 
         Map<String,String> map = new HashMap<>();
         int index1 = 0;
